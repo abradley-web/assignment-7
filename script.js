86c0d5565b9ffa569f90ef2a5be88f38c191f20c
abradley-web <abradley4005@gmail.com>
2025-10-15 11:24:21 -0500

    Completed problems 1 and 2

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 console.log("Hello World!");
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
 
-
+let favoriteFoods = ["pizza", "chargrilled oysters", "beingets", "crawfish", "pastalaya", "ice cream"];
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
 
-
+for (let i = 0; i < favoriteFoods.length; i++) {
+    console.log("One of my favorite foods is " + favoriteFoods[i] + ".");
+}
 
 
 
